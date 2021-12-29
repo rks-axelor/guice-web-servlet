9f70c6dae84a3b02f0a6896f566958c574ebbc72
ritesh <srkritesh001@gmail.com>
2021-12-29 15:37:14 +0530

    final commit

--- app/src/main/java/guice/web/servlet/demo/LoginServlet.java
+++ app/src/main/java/guice/web/servlet/demo/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.inject.Singleton;
+@Singleton
 public class LoginServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 6792868865271939154L;
